59ead2f35b889172e2e7a852de618f060ae42f7b
Francisco Carvalho <f.thiene@gmail.com>
2014-10-17 20:06:31 -0300

    Fix build and adds gitignore

--- build_appengine.py
+++ build_appengine.py
@@ -5,20 +5,15 @@ import sublime, sublime_plugin
 
 class AppEngineBuildCommand(sublime_plugin.WindowCommand):
   def run(self, paths = []):
-    sublime.status_message("Initializing...")
-
-    settings = sublime.load_settings('AppengineBuild/AppengineBuild.sublime-settings')
+    settings = sublime.load_settings('AppengineBuild.sublime-settings')
 
     project_path = settings.get('project_path')
     project_name = settings.get('project_name')
-    project_name_camelized = project_name.split("_").each {|s| s.capitalize! }.join("")
+    project_name_camelized = ''.join([item.capitalize() for item in project_name.split("_")])
 
     os.chdir(project_path)
-
-    sublime.status_message("Compiling project...")
     os.system("endpointscfg.py get_client_lib java -bs gradle " + project_name + "." + project_name_camelized)
 
-    sublime.status_message("Extracting files...")
     to_dir = "extracted-files"
     for file in glob.glob("*.zip"):
       zip = zipfile.ZipFile(file)
@@ -27,9 +22,7 @@ class AppEngineBuildCommand(sublime_plugin.WindowCommand):
 
     folder = os.listdir(os.getcwd() + "/" + to_dir)[0]
     os.chdir(to_dir + "/" + folder)
-    sublime.status_message("Running gradle install...")
     os.system("gradle install")
-    sublime.status_message("Removing tmp files...")
     os.chdir("../../")
     shutil.rmtree(to_dir)
-    sublime.status_message("Done!")
+    sublime.status_message("Build finished!")

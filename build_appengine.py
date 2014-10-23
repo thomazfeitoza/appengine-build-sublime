5edc41d9349a640f8fcb377e21ab9ce30d85f886
Thomaz <thomaz@Thomazs-Mac-Pro.local>
2014-10-23 10:30:54 -0700

    Creates command classes to build all and build for iOS. Adds threading to avoid sublime lock.

--- build_appengine.py
+++ build_appengine.py
@@ -1,7 +1,7 @@
 #coding: utf-8
 
 import sys, os, glob, zipfile, shutil
-import sublime, sublime_plugin, threading
+import sublime, sublime_plugin, threading, re, platform
 
 def get_members(zip):
     parts = []
@@ -18,21 +18,67 @@ def get_members(zip):
             zipinfo.filename = name[offset:]
             yield zipinfo
 
-class AppEngineBuildCommand(sublime_plugin.WindowCommand):
+
+class AppEngineEndpointsBuildAllCommand(sublime_plugin.WindowCommand):
   def run(self, paths = []):
+    build_thread = AppEngineEndpointsBuildThread(paths=paths, android=True, ios=True)
+    build_thread.start()
+
+  def is_enabled(self, paths = []):
+    return True
+
+
+class AppEngineEndpointsBuildAndroidCommand(sublime_plugin.WindowCommand):
+  def run(self, paths = []):
+    build_thread = AppEngineEndpointsBuildThread(paths=paths, android=True)
+    build_thread.start()
+
+  def is_enabled(self, paths = []):
+    return True
+
+
+class AppEngineEndpointsBuildIosCommand(sublime_plugin.WindowCommand):
+  def run(self, paths = []):
+    build_thread = AppEngineEndpointsBuildThread(paths=paths, ios=True)
+    build_thread.start()
+
+  def is_enabled(self, paths = []):
+    match = re.search(r'darwin', platform.system(), re.I)
+    return match != None
+
+
+
+class AppEngineEndpointsBuildThread(threading.Thread):
+
+  def __init__(self, paths=[], android=False, ios=False):
+    self.paths = paths
+    self.android = android
+    self.ios = ios
+    threading.Thread.__init__(self)
+
+  def run(self):
+    if(self.android):
+      self.buildAndroid()
+
+    if(self.ios):
+      self.buildIOS()
+
+
+  def buildAndroid(self):
     settings = sublime.load_settings('AppEngineBuild.sublime-settings')
 
-    endpointscfg = settings.get('endpointscfg')
-    gradle = settings.get('gradle')
+    endpointscfg = settings.get('tools').get('endpointscfg')
+    gradle = settings.get('tools').get('gradle')
 
-    project_path = paths[0] 
+    project_path = self.paths[0] 
     settings.get(project_path)
     if settings.get(project_path) != None:
-      project_name = settings.get(project_path).get('project_name')
-      api_name = settings.get(project_path).get('api_name')
+
+      apis = settings.get(project_path).get('apis')
+      apis_str = ' '.join(api.get('module') + '.' + api.get('class') for api in apis)
 
       os.chdir(project_path)
-      os.system(endpointscfg+" get_client_lib java -bs gradle " + project_name + "." + api_name)
+      os.system(endpointscfg+" get_client_lib java -bs gradle " + apis_str)
 
       to_dir = "extracted-files"
       for file in glob.glob("*.zip"):
@@ -47,3 +93,7 @@ class AppEngineBuildCommand(sublime_plugin.WindowCommand):
       sublime.status_message("Build finished!")
     else:
       sublime.error_message("Project \""+project_path+"\" not found in \"AppEngine Build -> user.settings\"")
+
+
+  def buildIOS(self):
+    sublime.status_message("iOS Build finished!")

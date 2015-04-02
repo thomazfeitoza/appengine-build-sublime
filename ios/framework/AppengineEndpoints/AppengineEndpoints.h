1b4d3d3a5d40ada9f574a41ffdea8f59783c58f2
Thomaz Feitoza <thomazfeitoza@gmail.com>
2015-04-02 17:18:20 -0300

    Creates ios base framework.

---
+++ ios/framework/AppengineEndpoints/AppengineEndpoints.h
@@ -0,0 +1,13 @@
+//
+//  AppengineEndpoints.h
+//  AppengineEndpoints
+//
+//  Created by Thomaz Feitoza on 02/04/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppengineEndpoints : NSObject
+
+@end
